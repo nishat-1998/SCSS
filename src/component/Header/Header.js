76b423f7e80690bb218b62581f6ca56e42590cfc
Nishat Ahmed <nishatahmed4485@gmail.com>
2021-07-12 14:26:23 +0600

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import  './Header.scss';
+const Header = () => {
+    return (
+        <div>
+            <h1>hello How are you?</h1>
+            <div className="first">
+                <h1>Rohan </h1>
+            </div>
+            <div>
+                <h1 className="second">Rashed </h1>
+            </div>
+            <div className="three">
+                <h1>RAzu </h1>
+            </div>
+            <div className="four">
+                <h1>Rakib </h1>
+            </div>
+        </div>
+        
+    );
+};
+
+export default Header;
---
+++ src/component/Home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.scss'
+
+const home = () => {
+    return (
+        <div className="home-container">
+            <p>THIS IS HOME</p>
+        </div>
+    );
+};
+
+export default home;
